9e96b631f85f1def1bec6f2eed11a7c409a8380c
sarateanud2 <sarateanud2@gmail.com>
2017-01-27 12:58:32 +0300

    Update CommThread.java

--- Thread2/src/com/thread/thread/CommThread.java
+++ Thread2/src/com/thread/thread/CommThread.java
@@ -16,18 +16,3 @@ public class CommThread implements Runnable {
 	}
 
 }
-
-
-/*
- synchronized(res){
-            res.x=1;
-            for (int i = 1; i < 5; i++){
-                System.out.printf("%s %d \n", Thread.currentThread().getName(), res.x);
-                res.x++;
-                try{
-                    Thread.sleep(100);
-                }
-                catch(InterruptedException e){}
-            }
-        }
- */
